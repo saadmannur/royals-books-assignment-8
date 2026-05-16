b01633075f6ecd3b11e190b52f4ab5be5f688e3a
saadmannur <saadmannur99@gmail.com>
2026-05-16 22:58:26 +0600

    banner, navbar, footer added

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -6,12 +6,12 @@ import { FaArrowRight } from 'react-icons/fa';
 
 const Banner = () => {
     return (
-        <div className=' bg-[#d0e1e7] px-10 pt-10'>
+        <div className=' bg-[#d0e1e7] md:px-10 pt-10'>
             <div className='container mx-auto flex justify-between items-center'>
                 <div className='text-center'>
-                    <h2 className='text-[#012e4a] text-5xl font-bold'>Find Your Next Read</h2>
+                    <h2 className='text-[#012e4a] text-2xl md:text-5xl font-bold'>Find Your Next Read</h2>
                     <Link href={"/allbooks"} className='flex items-center justify-center my-5'>
-                        <button className='bg-[#036280] text-white flex items-center justify-center gap-2 p-3 rounded-full hover:bg-orange-900 duration-1000'><span className='text-2xl pb-1'>Browse Now</span> <span className=''><FaArrowRight /></span></button>
+                        <button className='bg-[#036280] text-white flex items-center justify-center gap-2 p-1 md:p-3 rounded-full hover:bg-orange-900 duration-800'><span className='md:text-2xl pb-1'>Browse Now</span> <span className=''><FaArrowRight /></span></button>
                     </Link>
                 </div>
                 <div >
---
+++ src/components/homepage/Marquee.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Marquee from "react-fast-marquee";
+
+const MarqueePage = () => {
+    return (
+        <div>
+            <Marquee>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis consequatur rerum placeat dignissimos. Asperiores accusantium quia optio odio provident aspernatur magnam nobis inventore vitae, error modi quidem? Ipsum voluptates perferendis suscipit doloremque quasi aperiam itaque labore dolorem commodi? Numquam corporis modi cum odit, maxime ad similique aliquam suscipit, eum corrupti error totam non earum enim quisquam velit maiores veritatis sint quibusdam ratione quo! Minima nihil fugiat necessitatibus autem velit rem aut, laudantium eaque veritatis ab. Debitis consequatur quo eum quod aliquam nesciunt itaque magnam, dolore, accusantium asperiores deleniti numquam illo maiores! Id vitae modi aliquid iste reiciendis, quis ad ea impedit aspernatur, iure excepturi earum accusamus accusantium! Possimus sint ullam cupiditate! Ratione sunt reprehenderit, placeat repudiandae unde beatae et dolorem maxime, delectus totam corrupti laboriosam eum nihil debitis sed fugit aliquid excepturi? Eaque molestias aliquam accusamus magni minus laborum consequatur laudantium eum sequi explicabo officia, fuga rem repudiandae saepe dolore expedita assumenda. Ab ducimus recusandae soluta facere quam? Consectetur, officiis temporibus laudantium autem esse voluptate nam ipsum iusto ratione in quam saepe sint molestias, mollitia incidunt veniam at ipsam suscipit totam ut nisi expedita. Repudiandae, molestias totam incidunt aspernatur quibusdam, obcaecati fugit corrupti cupiditate, esse pariatur accusamus ratione rerum. Error.
+            </Marquee>
+        </div>
+    );
+};
+
+export default MarqueePage;
